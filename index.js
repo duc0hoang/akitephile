213bd1b152a3e334022490b25438f9b004acf15c
nguyen duc hoang <hoangnd5@scb.com.vn>
2022-11-21 01:09:04 +0700

    21 11 2022

--- index.js
+++ index.js
@@ -224,3 +224,9 @@ function initMobile() {
 function onChangeMobileMenu(){
     $('#mobile-menu').toggleClass("open");
 }
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
